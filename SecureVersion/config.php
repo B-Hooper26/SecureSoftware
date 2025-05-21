8a94b5096d1e264a37b5cadd6dfaf25ed0da6774
ben <benhoop26@gmail.com>
2025-05-21 10:39:58 +0100

    Workflow test

--- SecureVersion/config.php
+++ SecureVersion/config.php
@@ -1,20 +1,20 @@
 <?php
 // Database configuration
-$config = [
-//UNCOMENT THIS TO DO SECURITY TESTS AND COMMENT THE CONFIG CODE BELOW IT
-    'DB_SERVER' => '127.0.0.1',
-    'DB_USERNAME' => 'root', 
-    'DB_PASSWORD' => 'root',
-    'DB_NAME' => 'securedatabase'
-];
-
-//COMMENT THIS OUT WHEN DOING SECURITY TESTS
 // $config = [
+// //UNCOMENT THIS TO DO SECURITY TESTS AND COMMENT THE CONFIG CODE BELOW IT
 //     'DB_SERVER' => '127.0.0.1',
-//     'DB_USERNAME' => 'root',
-//     'DB_PASSWORD' => '', // <--- empty string
+//     'DB_USERNAME' => 'root', 
+//     'DB_PASSWORD' => 'root',
 //     'DB_NAME' => 'securedatabase'
 // ];
+
+//COMMENT THIS OUT WHEN DOING SECURITY TESTS- you will not be able to access the wbeiste as this will perform the github actions.
+$config = [
+    'DB_SERVER' => '127.0.0.1',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => '', // <--- empty string
+    'DB_NAME' => 'securedatabase'
+];
 
 // Create connection
 $conn = new mysqli($config['DB_SERVER'], $config['DB_USERNAME'], $config['DB_PASSWORD'], $config['DB_NAME']);
